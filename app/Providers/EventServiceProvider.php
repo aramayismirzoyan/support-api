fa42f6920812e4faf7abc7156877036b294204ca
Aramayis <aramayismirzoyan@gmail.com>
2023-09-15 04:38:25 +0400

    Привязка события к слушателю

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Listeners\SendAnswerByEmail;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -25,7 +26,7 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Event::listen('App\Events\RequestAnswered', 'App\Listeners\SendAnswerByEmail');
     }
 
     /**
