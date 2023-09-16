323103bfbe48b64262675c0cfc894f8efd417004
Aramayis <aramayismirzoyan@gmail.com>
2023-09-16 05:01:48 +0400

    Перенос логику получение токена в модель пользователя

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Auth;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -49,5 +50,12 @@ class User extends Authenticatable
     public function requests(): HasMany
     {
         return $this->hasMany(Request::class);
+    }
+
+    public static function getToken($email)
+    {
+        return Auth::user()
+            ->createToken($email)
+            ->plainTextToken;
     }
 }
