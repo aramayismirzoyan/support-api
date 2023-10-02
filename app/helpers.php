8709125b004f33d5546f746b798af4acc91298c8
Aramayis <aramayismirzoyan@gmail.com>
2023-10-03 01:21:28 +0400

    Добавляем хелперы для удобной отправки ответов

--- app/helpers.php
+++ app/helpers.php
@@ -2,6 +2,7 @@
 
 use App\Models\Request;
 use App\Models\User;
+use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Support\Facades\Auth;
 
 if (!function_exists('createTestUser')) {
@@ -15,5 +16,40 @@ if (!function_exists('createRequest')) {
     function createRequest($data = [])
     {
         return Request::factory()->for(Auth::user())->create($data);
+    }
+}
+
+if (!function_exists('createForbiddenResponse')) {
+    function createForbiddenResponse(string $message = ''): \Illuminate\Http\JsonResponse
+    {
+        $response = ['success' => false];
+
+        if ($message !== '') {
+            $response['message'] = $message;
+        }
+
+        return response()->json($response, 403);
+    }
+}
+
+if (!function_exists('createSuccessResponse')) {
+    function createSuccessResponse(string $message = ''): \Illuminate\Http\JsonResponse
+    {
+        $response = ['success' => true];
+
+        if ($message !== '') {
+            $response['message'] = $message;
+        }
+
+        return response()->json($response);
+    }
+}
+
+if (!function_exists('throwForbiddenResponseException')) {
+    function throwForbiddenResponseException(string $message = ''): void
+    {
+        $response = createForbiddenResponse($message);
+
+        throw new HttpResponseException($response);
     }
 }
