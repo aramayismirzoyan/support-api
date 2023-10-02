f0b57104ca3e00e5d43615cd5a8bf037a0e1fb03
Aramayis <aramayismirzoyan@gmail.com>
2023-10-03 01:19:11 +0400

    Выносим логику в сервис

---
+++ app/Services/Request/RequestService.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Services\Request;
+
+use App\Filters\Request\RequestFilterFactory;
+use App\Models\Request as RequestModel;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class RequestService
+{
+    public function getFiltered(Request $request): Collection
+    {
+        $requestModel = RequestModel::query();
+        $filter = RequestFilterFactory::make();
+
+        $fields = ['id', 'status', 'message', 'answer', 'user_id', 'created_at'];
+
+        return $filter
+            ->query($requestModel, $request)
+            ->select($fields)
+            ->get();
+    }
+
+    public function create($message)
+    {
+        Auth::user()
+            ->requests()
+            ->create([
+                'message' => $message
+            ]);
+    }
+
+    public function addAnswer($answer, $requestModel): void
+    {
+        $requestModel->status = RequestModel::RESOLVED;
+        $requestModel->answer = $answer;
+        $requestModel->save();
+    }
+}
