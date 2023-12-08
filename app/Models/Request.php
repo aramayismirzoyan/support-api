ce59b9c5bf99d1601764103a3a5eb2b074d5b7b8
Aramayis <aramayismirzoyan@gmail.com>
2023-12-08 04:45:47 +0400

    Удаление лишних пространств имён

--- app/Models/Request.php
+++ app/Models/Request.php
@@ -2,12 +2,9 @@
 
 namespace App\Models;
 
-use Exception;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Gate;
 
 class Request extends Model
 {
